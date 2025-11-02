f429ce3c2ae61bee60b5b680dcab5b6317552124
Tarun-48 <taruncs04@gmail.com>
2025-11-02 23:50:05 +0530

    Update app.py1

--- app.py
+++ app.py
@@ -468,5 +468,9 @@ def delete_note(note_id):
     flash(f"🗑 Note '{note.title}' deleted successfully.", "info")
     return redirect(url_for('view_notes'))
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True)
